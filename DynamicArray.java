24d1d9697606604dbc1d53592776a942c599df41
VenkataRamana <167703269+venkat2k3@users.noreply.github.com>
2025-01-23 23:09:10 +0530

    Add files via upload

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,21 +1,20 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class DynamicArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements you want to store in the dynamic array: ");
-        int numElements = scanner.nextInt();
+        int n = scanner.nextInt();
         ArrayList<Integer> dynamicArray = new ArrayList<>();
-        System.out.println("Enter " + numElements + " elements:");
-        for (int i = 0; i < numElements; i++) {
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
             int element = scanner.nextInt();
             dynamicArray.add(element);
         }
         System.out.println("The elements of the dynamic array are: " + dynamicArray);
         System.out.print("Enter a new element to add to the array: ");
-        int newElement = scanner.nextInt();
-        dynamicArray.add(newElement);
+        int newE = scanner.nextInt();
+        dynamicArray.add(newE);
         System.out.println("The updated dynamic array is: " + dynamicArray);
         
         scanner.close();
